1dc9f649ca648d7dee5d773bc428abf6abbb36d7
Sam Dayo <samueldayo2017@gmail.com>
2023-10-30 15:18:13 -0400

    Printing to Uart

--- Lab_4/Core/Src/main.c
+++ Lab_4/Core/Src/main.c
@@ -51,7 +51,7 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c2;
 
-UART_HandleTypeDef huart2;
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 float humidity = 0.0;
@@ -62,6 +62,7 @@ char str_hum[100] = ""; //formatted string message to display the temperature
 char msg1[] = "***** Humidity measurements *****\n\n\r";
 char msg2[] = "=====> Initialize Humidity Sensor HT221 \r\n";
 char msg3[] = "=====> Humidity sensor initialized \r\n";
+uint8_t sensor = 0;
 
 
 /* USER CODE END PV */
@@ -70,17 +71,17 @@ char msg3[] = "=====> Humidity sensor initialized \r\n";
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C2_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int _write(int file, char *ptr, int len){
-	HAL_UART_Transmit(&huart2, (uint8_t*) ptr, len, HAL_MAX_DELAY);
-	return len;
-}
+//int _write(int file, char *ptr, int len){
+//	HAL_UART_Transmit(&huart1, (uint8_t*) ptr, len, HAL_MAX_DELAY);
+//	return len;
+//}
 /* USER CODE END 0 */
 
 /**
@@ -112,15 +113,15 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C2_Init();
-  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   // initialize the four sensors
 //  HAL_UART_Transmit(&huart1, msg1, sizeof(msg1), 1000);
 //  HAL_UART_Transmit(&huart1, msg2, sizeof(msg2), 1000);
   BSP_HSENSOR_Init();
-  //BSP_MAGNETO_Init();
-//  BSP_ACCELERO_Init();
-//  BSP_GYRO_Init();
+  BSP_MAGNETO_Init();
+  BSP_ACCELERO_Init();
+  BSP_GYRO_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -130,17 +131,37 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+	  // read all four sensors
 	  humidity = BSP_HSENSOR_ReadHumidity();
-	  int humInt1 = humidity;
-	  float humfrac = humidity - humInt1;
-	  int humInt2 = trunc(humfrac * 100);
-	  printf("I'm trying to get this shit working");
-	  //snprintf(str_hum,100," Humidity = %d.02%d\n\r", humInt1, humInt2);
-	 // HAL_UART_Transmit(&huart2, (uint8_t*) str_hum, sizeof(str_hum), 1000);
-//	  BSP_ACCELERO_AccGetXYZ(accel);
-//	  BSP_GYRO_GetXYZ(gyro);
-//	  BSP_MAGNETO_GetXYZ(magneto);
-	  HAL_Delay(1000);
+	  BSP_ACCELERO_AccGetXYZ(accel);
+	  BSP_GYRO_GetXYZ(gyro);
+	  BSP_MAGNETO_GetXYZ(magneto);
+
+	  switch(sensor){
+	  	  case 0:
+	  		  sprintf(str_hum, "The humidity is %d\n\r", (int)humidity);
+	  		  break;
+	  	  case 1:
+	  		  sprintf(str_hum, "The accelerometer reports that velocities are: x = %d, y = %d, z = %d\n\r", accel[0], accel[1], accel[2]);
+	  		  break;
+	  	  case 2:
+	  		 printf(str_hum, "The gyroscope reports that: x = %d, y = %d, z = %d\n\r",(int)gyro[0],(int)gyro[1],(int)gyro[2]);
+	  		 break;
+	  	  case 3:
+	  		 printf(str_hum, "The magnetometer reports: x = %d, y = %d, z = %d\n\r", magneto[0], magneto[1], magneto[2]);
+	  		 break;
+	  	  default:
+	  }
+
+	  HAL_UART_Transmit(&huart1, (uint8_t*) str_hum, sizeof(str_hum), HAL_MAX_DELAY);
+
+//	  int humInt1 = humidity;
+//	  float humfrac = humidity - humInt1;
+//	  int humInt2 = trunc(humfrac * 100);
+//	  sprintf(str_hum, "The humidity is %d\n\r", (int)humidity);
+//	  HAL_UART_Transmit(&huart1, (uint8_t*) str_hum, sizeof(str_hum), 1000);
+//	  HAL_Delay(1000);
 
 //	  printf("Hello World");
 //	  printf("\r\n");
@@ -248,50 +269,50 @@ static void MX_I2C2_Init(void)
 }
 
 /**
-  * @brief USART2 Initialization Function
+  * @brief USART1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_USART2_UART_Init(void)
+static void MX_USART1_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART2_Init 0 */
+  /* USER CODE BEGIN USART1_Init 0 */
 
-  /* USER CODE END USART2_Init 0 */
+  /* USER CODE END USART1_Init 0 */
 
-  /* USER CODE BEGIN USART2_Init 1 */
+  /* USER CODE BEGIN USART1_Init 1 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
+  /* USER CODE BEGIN USART1_Init 2 */
 
-  /* USER CODE END USART2_Init 2 */
+  /* USER CODE END USART1_Init 2 */
 
 }
 
@@ -302,18 +323,34 @@ static void MX_USART2_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin : pushbutton_Pin */
+  GPIO_InitStruct.Pin = pushbutton_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(pushbutton_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
+
+void HAL_GPIO_EXTI_Callback (uint16_t GPIO_Pin){
+	sensor = (sensor + 1)%4;
+
+}
 
 /* USER CODE END 4 */
 
